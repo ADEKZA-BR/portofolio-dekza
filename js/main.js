4eed0b01b8a3abfc182723453f33492ee1f40257
ADEKZA BACHRUDIN RIZKY <adekzabachrudinrizky1@gmail.com>
2025-12-13 18:26:42 +0700

    update: interfaces

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const roles = ["WEB DEVELOPER", "UI DESIGNER", "JUNIOR IT SUPPORT"];
+let index = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+  const current = roles[index];
+
+  if (!deleting) {
+    typing.textContent = current.substring(0, charIndex++);
+    if (charIndex > current.length) {
+      deleting = true;
+      return setTimeout(typeEffect, 1500);
+    }
+  } else {
+    typing.textContent = current.substring(0, charIndex--);
+    if (charIndex < 0) {
+      deleting = false;
+      index = (index + 1) % roles.length;
+      charIndex = 0;
+    }
+  }
+
+  setTimeout(typeEffect, deleting ? 60 : 90);
+}
+
+typeEffect();
